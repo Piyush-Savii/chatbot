70b48fdaf1b2205863bc17e252a39547331fa8da
Piyush <piyush.l.khadgi@gmail.com>
2025-09-03 15:09:12 +0100

    Mentioning app_mention 1.1

--- messages.py
+++ messages.py
@@ -92,8 +92,9 @@ def validate_event(body: Dict, user_manager) -> Union[Dict[str, str], None]:
 
     # Validate event type
     event = body.get("event", {})
-    if event.get("type") != "app_mention":
-        logger.info(f" in {func} 📥 Not an app_mention, but {event.get("type")}\n")
+    event_type = event.get("type")
+    if event_type != "app_mention":
+        logger.info(f" in {func} 📥 Not an app_mention, but {event_type}\n")
         return {"status": "error"}
 
     logger.info(f" in {func} 📥 Received Slack request: {body['event']['text']}\n")
